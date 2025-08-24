0f868393ba156d66fa928390f47df6b3e9d6cdec
Hyejinnnn2 <kong1015@g.skku.edu>
2025-08-24 16:40:27 +0900

    chore: add .env and use VITE_API_BASE

--- src/api/storeApi.js
+++ src/api/storeApi.js
@@ -1,13 +1,11 @@
 // src/api/storeApi.js
 import axios from 'axios';
 
+const BASE_URL = import.meta.env.VITE_API_BASE; // ← 환경변수로 받기
+
 export async function fetchStoreData(userId = 1) {
-  const BASE_URL =
-    'http://pickcoupon-app-env.eba-pkfhnrry.ap-northeast-2.elasticbeanstalk.com';
   const { data } = await axios.get(`${BASE_URL}/api/recommendations`, {
     params: { userId },
-    // withCredentials: false,        // (기본값 false지만 혹시 세팅했다면 꺼두기)
-    // headers: undefined,            // Accept/Content-Type도 굳이 안 보냄
   });
   return data; // { message, stores }
 }
